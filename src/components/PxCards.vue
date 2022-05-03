b2f4215e8228cc93dc7882581bcbd306cc9652e0
esvelpe <esneidervelpe@gmail.com>
2022-05-02 19:57:08 -0500

    Organización de las cards

--- src/components/PxCards.vue
+++ src/components/PxCards.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <b-card-group deck>
-      <b-card
-        v-for="ch in characters"
-        :key="ch.id"
-        :title="ch.name"
-        :img-src="ch.image"
-        img-top
-      ></b-card>
-    </b-card-group>
+    <b-container>
+      <b-row class="mt-md-3">
+        <b-col md="3" v-for="ch in characters" :key="ch.id">
+          <b-card
+            :title="ch.name"
+            :img-src="ch.image"
+            img-top
+            class="mb-md-3"
+          ></b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
