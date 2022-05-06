8c1827d8c1a9cb21515fc4ce90c1e3c3f29e759a
esvelpe <esneidervelpe@gmail.com>
2022-05-05 23:27:25 -0500

    Diseño del componente CharacterDetail

--- src/components/PxCards.vue
+++ src/components/PxCards.vue
@@ -3,24 +3,18 @@
     <b-container>
       <b-row class="m-md-2 p-md-5 justify-between">
         <b-col md="6" v-for="ch in charactersList" :key="ch.id">
-          <b-card
-            header-tag="header"
-            :title="ch.name"
-            :img-src="ch.image"
-            img-top
-            class="mb-md-3"
+          <router-link
+            :to="{ name: 'character-detail', params: { id: ch.id } }"
           >
-            <template #header>
-              <div
-                :class="{
-                  'bg-success': ch.status === 'Alive',
-                  'bg-danger': ch.status === 'Dead',
-                  'bg-secondary': ch.status === 'uknown',
-                }"
-                class="rounded-circle"
-              ></div>
-            </template>
-          </b-card>
+            <b-card
+              header-tag="header"
+              :title="ch.name"
+              :img-src="ch.image"
+              img-top
+              class="mb-md-3"
+            >
+            </b-card>
+          </router-link>
         </b-col>
       </b-row>
     </b-container>
--- src/views/CharacterDetail.vue
+++ src/views/CharacterDetail.vue
@@ -8,7 +8,24 @@
   </div>
 </template>
 <script>
+import api from "@/api.js";
+
 export default {
   name: "CharacterDetail",
+
+  data() {
+    return {
+      character: [],
+      episodes: [],
+      locations: [],
+    };
+  },
+
+  methods: {
+    getInfo() {
+      const id = this.$route.params.id;
+      Promise.all([api.getCharacter(id)]);
+    },
+  },
 };
 </script>
