d90e6cf2fbd23f00bedb2f645c9c8c2766b38f99
esvelpe <esneidervelpe@gmail.com>
2022-05-08 21:02:04 -0500

    Reparación error

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,3 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-
-const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin");
-
-// vue.config.js
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new GoogleFontsPlugin({
-        fonts: [{ family: "Macondo" }],
-      }),
-    ],
-  },
-};
