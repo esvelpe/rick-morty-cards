fff724e9fde4446509003efa06deab6e7ec63942
esvelpe <esneidervelpe@gmail.com>
2022-05-08 21:10:00 -0500

    Finalización del proyecto añadiendo footer con la información del desarrollador

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <px-pagination></px-pagination>
+    <footer>
+      <p class="d-flex justify-content-center">
+        Developed by:
+        <a href="https://github.com/esvelpe" target="_blank" class="pl-1">
+          Esneider Vélez Peña</a
+        >
+      </p>
+    </footer>
   </div>
 </template>
 
